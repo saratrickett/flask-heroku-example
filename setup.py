ca20e5a110df98391ba8bd99d59be491f204f7c1
saratrickett <strickett4@gmail.com>
2018-02-27 12:54:10 -0500

    Add heroku essential file

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-heroku-example',
+    packages=find_packages(),
+    version='0.1',
+    description='An example practice heroku flask application.',
+    author='Sara Trickett',
+    author_email='strickett4@gmail.com',
+    keywords=['dev3l', 'python', 'kata',],
+    install_requires=[
+        'pytest',
+        'flask',
+        'flask-runner',
+        'gunicorn',
+        'bandit',
+        'coverage',
+        'coveralls',
+        'pylint',
+        'pytest'
+
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+)
---
+++ venv/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_runner import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+if __name__== '__main__':
+    manager.run()

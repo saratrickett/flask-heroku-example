0585475a5bc7d0149fc167e3b3e071d36bf905fc
saratrickett <strickett4@gmail.com>
2018-03-06 13:02:44 -0500

    Add microblog flask application

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
+
 from app import app
+
+@app.route('/index1')
+def index1():
+    user = {'username': 'Sara'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Sara Trickett'}
+    return render_template('index.html', title='Home', user=user)

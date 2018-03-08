547131eefe8beadffb74466e4fd97a16ed057e74
saratrickett <strickett4@gmail.com>
2018-03-08 12:49:10 -0500

    Add to routes

--- app/routes.py
+++ app/routes.py
@@ -10,13 +10,34 @@ def index1():
     <head>
         <title>Home Page - Microblog</title>
     </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
+        <h1>Hello, '''
+    <body> + user['username'] + '''!</h1>
     </body>
 </html>'''
+
+
+@app.route('/index2')
+def index2():
+
+    user = {'username': 'Sara Trickett'}
+    return render_template('index.html', title='Home', user=user)
+
+
+from flask import render_template
+from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Sara Trickett'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
